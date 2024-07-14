5a21990811f3acf6c9bef88c8a81d89024b1962d
rishika-on-git <aggarwalrishika135@gmail.com>
2024-07-14 15:17:59 +0530

    committed 2

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <TextGenerateEffectDemo />
       <StickyScrollRevealDemo />
       <ParallaxScrollDemo/>
-      <CanvasRevealEffectDemo/>
+      {/* <CanvasRevealEffectDemo/> */}
       <InfiniteMovingCardsDemo />
       <BackgroundBeamsDemo />
      
