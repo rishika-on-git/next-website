3e468950e259b713e36d1e4e2f0fb4c87f147140
rishika-on-git <aggarwalrishika135@gmail.com>
2024-07-14 15:25:21 +0530

    committe 7

--- app/components/ui/text-generate-effect.tsx
+++ app/components/ui/text-generate-effect.tsx
@@ -45,7 +45,7 @@ export const TextGenerateEffect = ({
   return (
     <div className={cn("font-bold", className)}>
       <div className="mt-4 mb-4 ml-10 mr-10 p-10 sm:mx-auto">
-        <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
+        <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide sm:text-xs">
           {renderWords()}
         </div>
       </div>
